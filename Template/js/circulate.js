ea53d99f61a30cc87f62b37d317668ffc20aeede
FlorescuAndreea <andreeaflorescu_15@yahoo.com>
2013-12-10 12:53:24 +0200

    cerculeaua

--- Template/js/circulate.js
+++ Template/js/circulate.js
@@ -2,40 +2,53 @@
  * Created by andreea on 12/10/13.
  */
 $(window).load(function() {
-    function startBallOne() {
-        $("#orange-ball").circulate({
-            speed: 4000,
-            height: 100,
-            width: -700,
-            sizeAdjustment: 40,
-            loop: true,
-            zIndexValues: [1, 1, 3, 3]
+//    function startBallOne() {
+//        $("#orange-ball").circulate({
+//            speed: 4000,
+//            height: 100,
+//            width: -700,
+//            sizeAdjustment: 40,
+//            loop: true,
+//            zIndexValues: [1, 1, 3, 3]
+//        });
+//    }
+//
+//    function startBallTwo() {
+//        $("#blue-ball").circulate({
+//            speed: 4000,
+//            height: 120,
+//            width: -700,
+//            sizeAdjustment: 35,
+//            loop: true,
+//            zIndexValues: [2, 2, 2, 2]
+//        })
+//    }
+//
+//    function startBallThree() {
+//        $("#green-ball").circulate({
+//            speed: 4000,
+//            height: 140,
+//            width: -700,
+//            sizeAdjustment: 30,
+//            loop: true,
+//            zIndexValues: [3, 3, 1, 1]
+//        }).fadeIn();
+//    }
+//
+//    startBallOne();
+//    setTimeout(startBallTwo, 2000);
+//    setTimeout(startBallThree, 4000);
+    $(".top-demo div").each(function() {
+        $(this).circulate({
+            speed: Math.floor(Math.random()*300) + 100,
+            height: Math.floor(Math.random()*1000) - 470,
+            width: Math.floor(Math.random()*1000) - 470
         });
-    }
-
-    function startBallTwo() {
-        $("#blue-ball").circulate({
-            speed: 4000,
-            height: 120,
-            width: -700,
-            sizeAdjustment: 35,
-            loop: true,
-            zIndexValues: [2, 2, 2, 2]
-        })
-    }
-
-    function startBallThree() {
-        $("#green-ball").circulate({
-            speed: 4000,
-            height: 140,
-            width: -700,
-            sizeAdjustment: 30,
-            loop: true,
-            zIndexValues: [3, 3, 1, 1]
-        }).fadeIn();
-    }
-
-    startBallOne();
-    setTimeout(startBallTwo, 2000);
-    setTimeout(startBallThree, 4000);
+    }).click(function() {
+            $(this).circulate({
+                speed: Math.floor(Math.random()*300) + 100,
+                height: Math.floor(Math.random()*1000) - 470,
+                width: Math.floor(Math.random()*1000) - 470
+            });
+        });
 });
