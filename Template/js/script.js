0c65abdba59f2fcfada2f9f2037a6292dca0eaeb
Andreea Florescu <andreeaflorescu_15@yahoo.com>
2013-12-11 18:07:53 +0200

    document.ready event

--- Template/js/script.js
+++ Template/js/script.js
@@ -1,34 +1,35 @@
-var page = $("#root");
-var volume;
+$(document).ready(function(){
+    var page = $("#root");
+    var volume;
+    var logoCreater = function(width, startPoint){
+        $( "#colored-header div" ).each(function( index ) {
+            var element = $(this);
+            element.css("left", startPoint + "px" );
+            startPoint += width;
+        });
+    }
 
-var logoCreater = function(width, startPoint){
-    $( "#colored-header div" ).each(function( index ) {
-        var element = $(this);
-        element.css("left", startPoint + "px" );
-        startPoint += width;
+    logoCreater(30, 10);
+
+    page.find('.img-container').on('hover', function() {
+        var container = $(this);
+        container.find('.text').toggle();
     });
-}
 
-logoCreater(30, 10);
+    page.find('#colored-header div').on("mouseenter", function() {
+        var el = $(this);
+        //console.log(el);
+        el.height((parseFloat(el.height()) + 30) + 'px');
+        var audio = $("#" + $(this)[0].id + "-note");
+        audio[0].play();
+    });
 
-page.find('.img-container').on('hover', function() {
-    var container = $(this);
-    container.find('.text').toggle();
-});
-
-page.find('#colored-header div').on("mouseenter", function() {
-	var el = $(this);
-	//console.log(el);
-	el.height((parseFloat(el.height()) + 30) + 'px');
-    var audio = $("#" + $(this)[0].id + "-note");
-    audio[0].play();
-});
-
-page.find('#colored-header div').on("mouseleave", function() {
-	var el = $(this);
-	//console.log(el);
-	el.height((parseFloat(el.height()) - 30) + 'px');
-    var audio = $("#" + $(this)[0].id + "-note");
-    audio[0].pause();
-    audio[0].currentTime = 0;
+    page.find('#colored-header div').on("mouseleave", function() {
+        var el = $(this);
+        //console.log(el);
+        el.height((parseFloat(el.height()) - 30) + 'px');
+        var audio = $("#" + $(this)[0].id + "-note");
+        audio[0].pause();
+        audio[0].currentTime = 0;
+    });
 });
