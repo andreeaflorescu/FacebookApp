3c01923032a41af7d5b4e68a79ab1e9862523154
FlorescuAndreea <andreeaflorescu_15@yahoo.com>
2013-12-12 03:20:36 +0200

    added pages desing

--- parallax/js/script.js
+++ parallax/js/script.js
@@ -27,6 +27,24 @@ $(document).ready(function(){
         audio[0].pause();
         audio[0].currentTime = 0;
     });
+    // Disable scroll
+    // lock scroll position, but retain settings for later
+    var scrollPosition = [
+        self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+        self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+    ];
+    var html = jQuery('html'); // it would make more sense to apply this to body, but IE7 won't have that
+    html.data('scroll-position', scrollPosition);
+    html.data('previous-overflow', html.css('overflow'));
+    html.css('overflow', 'hidden');
+    window.scrollTo(scrollPosition[0], scrollPosition[1]);
+
+
+    // un-lock scroll position
+    var html = jQuery('html');
+    var scrollPosition = html.data('scroll-position');
+    html.css('overflow', html.data('previous-overflow'));
+    window.scrollTo(scrollPosition[0], scrollPosition[1])
     //initialise Stellar.js
     $(window).stellar();
 
