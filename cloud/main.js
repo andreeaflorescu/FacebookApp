bfd9d4c706fe6ff0c33e69e0e76a957a1eb585d5
FlorescuAndreea <andreeaflorescu_15@yahoo.com>
2013-12-15 13:33:20 +0200

    haabar n-am ce am mai modificat

--- cloud/main.js
+++ cloud/main.js
@@ -1,72 +1,47 @@
-
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
+//
+////Use Parse.Cloud.define to define as many cloud functions as you want.
+////For example:
 //Parse.Cloud.define("hello", function(request, response) {
 //  response.success("Hello world!");
 //});
-<!--FACEBOOK LOGIN-->
-
-
-
-
-var handleLogin = function() {
-    Parse.FacebookUtils.logIn(null, {
-        success: function(user) {
-            if (!user.existed()) {
-                alert("User signed up and logged in through Facebook!");
-            } else {
-                alert("User logged in through Facebook!");
-            }
-        },
-        error: function(user, error) {
-            alert("User cancelled the Facebook login or did not fully authorize.");
-        }
-    });
-}
-
 //
-//var handleFacbookLogin = function(e) {
-//    var self = this;
+////var query = new Parse.Query(Parse.Event);
+////query.find("*").then(function(results) {
+////    console.log(results);
+////});
 //
-//    $(".fb_content").hide();
-//    $(".facebook_button")
-//        .addClass("disabled")
-//        .empty()
-//        .spin({length: 5, radius: 5, lines: 8, width: 3, color: "#fff"});
+//Parse.Cloud.define("mata", function(request, response) {
+//    var Event = Parse.Object.extend("Event");
+//    var event_new = new Event();
+//    event_new.save({
+//        name:  'Party',
+//        description: '',
+//        location: 'Not Available...Maybe you should contact the user?',
+//        date:  "",
+//        from: '',
+//        dressCode:'none',
+//        people: 'Everybody lost count!'
+//    }, {
+//        success: function() {
+//            // The object was saved successfully.
+//            response.success("Email sent!");
+//        },
+//        error: function(gameScore, error) {
+//            // The save failed.
+//            // error is a Parse.Error with an error code and description.
+//            response.error("caca");
+//        }
+//    });
+//});
 //
-//    if (Parse.User.current()) {
-//        this.saveHighScore();
-//    } else {
-//        Parse.FacebookUtils.logIn(null, {
-//            success: function(user) {
-//                // If it's a new user, let's fetch their name from FB
-//                if (!user.existed()) {
-//                    // We make a graph request
-//                    FB.api('/me', function(response) {
-//                        if (!response.error) {
-//                            // We save the data on the Parse user
-//                            user.set("displayName", response.name);
-//                            user.save(null, {
-//                                success: function(user) {
-//                                    // And finally save the new score
-//                                    self.saveHighScore();
-//                                },
-//                                error: function(user, error) {
-//                                    console.log("Oops, something went wrong saving your name.");
-//                                }
-//                            });
-//                        } else {
-//                            console.log("Oops something went wrong with facebook.");
-//                        }
-//                    });
-//                    // If it's an existing user that was logged in, we save the score
-//                } else {
-//                    self.saveHighScore();
-//                }
-//            },
-//            error: function(user, error) {
-//                console.log("Oops, something went wrong.");
-//            }
-//        });
-//    }
-//}
+//var page = $(document);
+//page.on('click', '#mata',function(){
+//    Parse.Cloud.run('mata', {}, {
+//        success: function(result) {
+//            // result is 'Hello world!'
+//        },
+//        error: function(error) {
+//        }
+//    });
+//
+//});
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,5 @@
+//var ev = require('cloud/main.js');
+
 $(document).ready(function(){
     var page = $(document);
 
@@ -130,13 +132,41 @@ $(document).ready(function(){
         })
     });
 
+    function makeScrollbar(content,scrollbar,handle,horizontal,ignoreMouse){
+        var steps = (horizontal?(content.getScrollSize().x - content.getSize().x):(content.getScrollSize().y - content.getSize().y))
+        var slider = new Slider(scrollbar, handle, {
+            steps: steps,
+            mode: (horizontal?'horizontal':'vertical'),
+            onChange: function(step){
+                // Scrolls the content element in x or y direction.
+                var x = (horizontal?step:0);
+                var y = (horizontal?0:step);
+                content.scrollTo(x,y);
+            }
+        }).set(0);
+        if( !(ignoreMouse) ){
+            // Scroll the content element when the mousewheel is used within the
+            // content or the scrol element.
+            $$(content, scrollbar).addEvent('mousewheel', function(e){
+                e = new Event(e).stop();
+                var step = slider.step - e.wheel * 30;
+                slider.set(step);
+            });
+        }
+        // Stops the handle dragging process when the mouse leaves the document body.
+        $(document.body).addEvent('mouseleave',function(){slider.drag.stop()});
+    }
+
+
 
 
 
 
 //    ========================================================FACEBOOK================================================
 
+        var writeEvent = function() {
 
+        }
 
 
 
